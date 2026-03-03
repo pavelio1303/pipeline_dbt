4ac542b236224146ee914d3a80a4b561ebbbb2ae
Pablo Jiménez Sánchez <pabloJimenez910.alu@stemgranada.com>
2026-03-03 18:25:53 +0000

    Ejercicio de pipeline en proceso

--- models/staging/erp/stg_erp__envios.sql
+++ models/staging/erp/stg_erp__envios.sql
@@ -1,7 +1,21 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='envio_id',
+    incremental_strategy='merge'
+  )
+}}
+
 with source as (
     select * from {{ source('bronze', 'BRZ_ERP_ENVIO') }}
+
+    {% if is_incremental() %}
+      -- Filtro incremental para comparar origen vs destino
+      where try_to_timestamp(fecha_hora_envio) > (select max(enviado_at) from {{ this }})
+    {% endif %}
 ),
-renamed as (
+
+renamed_cleaned as (
     select
         upper(trim(erp_id_envio)) as envio_id,
         upper(trim(erp_id_pedido)) as erp_pedido_id,
@@ -9,7 +23,10 @@ renamed as (
         upper(trim(numero_seguimiento)) as tracking_nro,
         upper(trim(pais_destino)) as pais_destino,
         upper(trim(estado_envio)) as estado_envio,
+
         try_to_timestamp(fecha_hora_envio) as enviado_at
+
     from source
 )
-select * from renamed
+
+select * from renamed_cleaned
